376844b6abbe4ec1cd757436a2bfbe2d7ae6f6b0
Vladimir Hrebtov <redshuhart015@gmail.com>
2020-04-07 12:16:21 +0300

    fix

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ var expressAppConfig = oas3Tools.expressAppConfig(path.join(__dirname, 'api/open
 expressAppConfig.addValidator();
 var app = expressAppConfig.getApp();
 
-app.use(cors());
+
 app.use(function(req, res, next) {
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
